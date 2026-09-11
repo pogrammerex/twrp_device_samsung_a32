fcd3a4d8f3d225430a047427981bbb2eb69df93f
pogrammerex <pogrammerex@gmail.com>
2026-09-11 18:34:55 +0300

    Update CI_config.sh

--- CI_config.sh
+++ CI_config.sh
@@ -3,7 +3,7 @@
 # Device
 export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
 export TWRP_BRANCH="twrp-12.1"
-export DT_LINK="https://github.com/almondnguyen/twrp_device_samsung_a32 -b twrp-12.1"
+export DT_LINK="https://github.com/Luminous418/twrp_device_samsung_a32 -b twrp-12.1"
 
 export DEVICE="a32"
 export OEM="samsung"
